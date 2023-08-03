41920d36102c1b2592e3603211b6db8deef7b37e
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2023-08-04 02:55:08 +0700

    Pengukuran bobot dan keparahan

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from preprocess import Preprocess
 from process import Process
 
 app = Flask(__name__)
 CORS(app)
 
-preprocess = Preprocess()
 process = Process()
 
 @app.route('/hello', methods=['GET'])
@@ -16,30 +14,18 @@ def hello():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    params = request.args
     body = request.get_json()
-    inputs = body.get('inputs')
-    tokenized = preprocess.preprocessing(inputs)
-    predictions = process.rounded_predictions(tokenized)
-    print(predictions)
-    return jsonify({'data': "OK"})
 
-@app.route('/summary-predict', methods=['POST'])
-def summary_predict():
-    body = request.get_json()
-    inputs = body.get('inputs')
-    tokenized = preprocess.preprocessing(inputs)
-    predictions = process.summary_predict(tokenized)
-    print(predictions)
-    return jsonify({'data': "OK"})
-    
-@app.route('/predict-token', methods=['POST'])
-def predict_token():
-    body = request.get_json()
-    inputs = body.get('inputs')
-    tokenized = preprocess.preprocess_get_token(inputs)
-    return jsonify({'data': {
-        'tokenized': tokenized,
-    }})
+    if params.get('type') == "ids":
+        inputs = body.get('inputs')
+        predictions = process.rounded_predictions(inputs)
+        return jsonify({'data': predictions})
+    else:
+        labels = body.get('labels')
+        inputs = body.get('inputs')
+        predictions = process.predict(labels, inputs)
+        return jsonify({'data': predictions})
 
 if __name__ == '__main__':
     app.run(debug=False)
--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from transformers import TFBertModel
+from preprocess import Preprocess
 
 BERT_NAME = 'indobenchmark/indobert-lite-base-p1'
 MODEL_PATH = 'model_3.keras'
@@ -12,39 +13,41 @@ class Process():
             custom_objects={'TFBertModel': TFBertModel.from_pretrained(BERT_NAME)},
             compile=False
         )
+        self.preprocess = Preprocess()
         self.threshold = 0.5
 
     def rounded_predictions(self, inputs):
-        predictions = self.model.predict(inputs)
-        return np.where(predictions > self.threshold, 1, 0)
+        tokenized = self.preprocess.preprocessing(inputs)
+        predictions = self.model.predict(tokenized)
+        return np.where(predictions > self.threshold, 1, 0).tolist()
 
-    def measure_severity(self, predictions):
-        return {
-            'label': 'Parah',
-            'index': 0.8,
-        }
+    def predict(self, labels, inputs):
+        predictions = self.rounded_predictions(inputs)
+        
+        outputs = []
 
-    def predict(self, inputs):
-        example_output = [0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0]
-        predictions = example_output
-        return {
-            'outputs': predictions,
-            'severity': self.measure_severity(predictions)
-        }
+        for i in range(len(predictions)):
+            prediction = predictions[i]
+            total_bobot = 0
+            total_masalah = 0
+            label_detected = []
 
-    def summary_predict(self, inputs):
-        example_output = [
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
-            [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0],
-        ]
-        # predictions = self.model.predict(inputs)
-        predictions = example_output
+            for j in range(len(prediction)):
+                if prediction[j] == 1:
+                    total_bobot += labels[j]['bobot']
+                    total_masalah += 1
+                    label_detected.append(labels[j]['label'])
+
+            output = {
+                'laporan': inputs[i],
+                'label': label_detected,
+                'prediction': prediction,
+                'severity': total_bobot / total_masalah,
+                'accuracy': 0
+            }
+            outputs.append(output)
+
         return {
-            'outputs': predictions,
-            'severity': self.measure_severity(predictions)
+            'outputs': outputs,
+            'sum_severity': sum([output['severity'] for output in outputs]) / len(outputs)
         }
